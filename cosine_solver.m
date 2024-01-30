b56e0ff0171b33589efe27c36909c06d026bb241
amhclark <amhclark@mun.ca>
2024-01-29 22:48:58 -0330

    Update

--- cosine_solver.m
+++ cosine_solver.m
@@ -2,25 +2,25 @@
 % Memorial University of Newfoundland
 % Faculty of Engineering & Applied Science
 
-% clear;
-% close all;
-% clc;
+clear;
+close all;
+clc;
 
-global sigma_x sigma_y sigma_z tau_xy tau_yz tau_zx sigma_i
+global sigma_x sigma_y sigma_z tau_xy tau_yz tau_zx 
 syms l_i m_i n_i
 
-% prompt = {'sigma_x','sigma_y','sigma_z','tau_xy','tau_yz','tau_zx','sigma_i'};
-% dlgtitle = 'Input';
-% fieldsize = [1 45; 1 45; 1 45; 1 45; 1 45; 1 45; 1 45];
-% % definput = {'20','hsv'};
-% answer = inputdlg(prompt,dlgtitle,fieldsize);
-% 
-% sigma_x = str2double(answer{1});
-% sigma_y = str2double(answer{2});
-% sigma_z = str2double(answer{3});
-% tau_xy = str2double(answer{4});
-% tau_yz = str2double(answer{5});
-% tau_zx = str2double(answer{6});
+prompt = {'sigma_x','sigma_y','sigma_z','tau_xy','tau_yz','tau_zx'};
+dlgtitle = 'Input';
+fieldsize = [1 45; 1 45; 1 45; 1 45; 1 45; 1 45];
+% definput = {'20','hsv'};
+answer = inputdlg(prompt,dlgtitle,fieldsize);
+
+sigma_x = str2double(answer{1});
+sigma_y = str2double(answer{2});
+sigma_z = str2double(answer{3});
+tau_xy = str2double(answer{4});
+tau_yz = str2double(answer{5});
+tau_zx = str2double(answer{6});
 % sigma_i = str2double(answer{7});
 
 stress = [sigma_x tau_xy tau_zx; tau_xy sigma_y tau_yz; tau_zx tau_yz sigma_z];
@@ -32,13 +32,13 @@ end
 
 stress*lmn(:,1) - lmn(:,1)*eigvals(1,1);
 
-for i =1:length(eigvals)
-    eq1 = (sigma_x - sigma_i)*l_i + tau_xy*m_i + tau_zx*n_i == 0;
-    eq2 = tau_xy*l_i + (sigma_y - sigma_i)*m_i + tau_yz*n_i == 0;
-    eq3 = tau_zx*l_i + tau_yz*m_i + (sigma_z - sigma_i)*n_i == 0;
-    eq4 = l_i^2 + m_i^2 + n_i^2 == 1;
-
-    [A,B] = equationsToMatrix([eq1, eq2, eq3, eq4], [l_i, m_i, n_i]);              
-    X = linsolve(A,B);
-end
+% for i =1:length(eigvals)
+%     eq1 = (sigma_x - sigma_i)*l_i + tau_xy*m_i + tau_zx*n_i == 0;
+%     eq2 = tau_xy*l_i + (sigma_y - sigma_i)*m_i + tau_yz*n_i == 0;
+%     eq3 = tau_zx*l_i + tau_yz*m_i + (sigma_z - sigma_i)*n_i == 0;
+%     eq4 = l_i^2 + m_i^2 + n_i^2 == 1;
+% 
+%     [A,B] = equationsToMatrix([eq1, eq2, eq3, eq4], [l_i, m_i, n_i]);              
+%     X = linsolve(A,B);
+% end
 
