f9ba04759a4d960b9d3816cbed7df85332442bb4
amhclark <amhclark@mun.ca>
2024-03-20 11:56:44 -0230

    Fracture toughness update

--- calculator.m
+++ calculator.m
@@ -1,7 +1,6 @@
 %% Fatigue & Fracture Mechanics
 %   Term Project
 %   Aidan Clark & Patrick Cleary
-
 clear
 close all
 clc
@@ -39,13 +38,16 @@ stress_analysis_type = input('Select Comparison Option (1 for Maximum Shear, 2 f
 % Input Data Unit Conversion
 yield_strength = 10e-6 * yield_strength_MPa;
 k_Ic = k_Ic_MPa * 10e-6;
-k_IIc = k_IIc_MPa * 10e-6;
+% k_IIc = k_IIc_MPa * 10e-6;
 k_IIIc = k_IIIc_MPa * 10e-6;
+
 
 
 %% Calculations
 
 % Geometric Calculations
+alph = a/radius;
+beta = 1 - alph;
 area = pi * radius^2;
 polar_moment_of_inertia = pi/32 * radius^4;
 
@@ -78,10 +80,26 @@ end
 % Safety Factor against yielding
 safety_factor_yielding_actual = yield_strength / abs(max(principal_stresses));
 
+% Fracture Mechanics
+s_g_axial = axial_force/(pi*radius^2);
+F_axial = (1/2*beta^1.5)*(1 + 0.5*beta + (3/8)*beta^2 - 0.363*beta^3 + 0.731*beta^4);
+k_axial = s_g_axial*F_axial*sqrt(pi*a);
+
+s_g_bending = (4*bending_moment)/(pi*radius^3);
+F_bending = (3/(8*beta^2.5))*(1 +0.5*beta + (3/8)*beta^2 + (5/16)*beta^3 + (35/128)*beta^4 + 0.537*beta^5);
+k_bending = s_g_bending*F_bending*sqrt(pi*a);
+
+s_g_torsion = (2*torque)/(pi*radius^3);
+F_torsion = (3/(8*beta^2.5))*(1 + 0.5*beta + (3/8)*beta^2 + (5/16)*beta^3 + (35/128)*beta^4 + 0.537*beta^5);
+k_torsion = s_g_torsion*F_torsion*sqrt(pi*a);
+
+
+k_I = k_axial + k_bending;
+k_III = k_torsion;
+
 % Safety Factors against fracture
-safety_factor_fracture_I_actual = k_Ic / abs(min(principal_stresses));
-safety_factor_fracture_II_actual = k_IIc / abs(min(principal_stresses));
-safety_factor_fracture_III_actual = k_IIIc / abs(min(principal_stresses));
+safety_factor_fracture_I = k_Ic / k_I;
+safety_factor_fracture_III = k_IIIc / k_III;
 
 
 
@@ -104,14 +122,9 @@ fprintf(output_file, 'Principal Stresses (Pa):\n');
 fprintf(output_file, '%f\n', principal_stresses);
 fprintf(output_file, 'Effective Stress (Pa): %f\n', effective_stress);
 fprintf(output_file, 'Safety Factor against Yielding: %f\n', safety_factor_yielding_actual);
-fprintf(output_file, 'Safety Factor against Fracture Mode I: %f\n', safety_factor_fracture_I_actual);
-fprintf(output_file, 'Safety Factor against Fracture Mode II: %f\n', safety_factor_fracture_II_actual);
-fprintf(output_file, 'Safety Factor against Fracture Mode III: %f\n', safety_factor_fracture_III_actual);
+fprintf(output_file, 'Safety Factor against Fracture Mode I: %f\n', safety_factor_fracture_I);
+fprintf(output_file, 'Safety Factor against Fracture Mode III: %f\n', safety_factor_fracture_III);
 fclose(output_file);
-
-% Fracture toughness comparison
-fracture_toughness_values = [k_Ic, k_IIc, k_IIIc];
-critical_stresses = fracture_toughness_values / safety_factor_fracture_I_actual;
 
 % Display fracture toughness comparison
 disp('----- Fracture Toughness Comparison -----');
